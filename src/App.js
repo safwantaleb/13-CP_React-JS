d1acf85d8e8ea8ad5850db8a0399206ea331f119
safwantaleb <safwan.taleb@gmail.com>
2021-03-05 17:19:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Button, Form, Col, Nav, Navbar, FormControl } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.css";
+
 function App() {
   return (
     <div className="App">
