800452f0fa02a9a8f4765db28703c3128b708337
safwantaleb <safwan.taleb@gmail.com>
2021-03-05 16:41:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Button, Form, Col, Nav, Navbar, FormControl } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
@@ -27,77 +27,82 @@ function App() {
           <Button variant="outline-primary">Search</Button>
         </Form>
       </Navbar>
-      <header className="App-header">
-        <Form>
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+
+      <header>
+        <div className="App-header">
+          <div className="bckimg"></div>
+          <br></br>
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+            <Form.Text className="text-muted">
+              We'll never share your email or any other information with anyone
+              else.
+            </Form.Text>
+            <Form.Group controlId="formGridPhone">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control placeholder="+216 xx xxx xxx" />
+            </Form.Group>
+            <Form.Group controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control placeholder="1234 Main St" />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </Form.Row>
-          <Form.Text className="text-muted">
-            We'll never share your email or any other information with anyone
-            else.
-          </Form.Text>
-          <Form.Group controlId="formGridPhone">
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control placeholder="+216 xx xxx xxx" />
-          </Form.Group>
-          <Form.Group controlId="formGridAddress1">
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
-          </Form.Group>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control />
+              </Form.Group>
 
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control />
-            </Form.Group>
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>State</Form.Label>
+                <Form.Control as="select" defaultValue="Choose...">
+                  <option>Choose...</option>
+                  <option>Tunis</option>
+                  <option>Sousse</option>
+                  <option>Sfax</option>
+                  <option>Gafsa</option>
+                </Form.Control>
+              </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>State</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>Tunis</option>
-                <option>Sousse</option>
-                <option>Sfax</option>
-                <option>Gafsa</option>
-              </Form.Control>
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Your Motivation</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Write here!" />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control />
+            <Form.Group as={Col} controlId="formGridFile">
+              <Form.Label>Import your CV</Form.Label>
+              <Form.File id="exampleFormControlFile1" />
             </Form.Group>
-          </Form.Row>
 
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Your Motivation</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Write here!" />
-          </Form.Group>
+            <Form.Group as={Col} id="formGridCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridFile">
-            <Form.Label>Import your CV</Form.Label>
-            <Form.File id="exampleFormControlFile1" />
-          </Form.Group>
-
-          <Form.Group as={Col} id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-          <Button variant="danger" type="reset">
-            Reset
-          </Button>
-        </Form>
-        <br></br>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            <Button variant="danger" type="reset">
+              Reset
+            </Button>
+          </Form>
+          <br></br>
+        </div>
       </header>
 
       <div class="d-flex flex-column">
