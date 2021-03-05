b42e616b348e2dc62c84498e6d02c8ce7959ca90
safwantaleb <78022970+safwantaleb@users.noreply.github.com>
2021-03-05 18:11:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -105,13 +105,13 @@ function App() {
         </div>
       </header>
 
-      <div class="d-flex flex-column">
-        <footer class="footer">
+      <div className="d-flex flex-column">
+        <footer className="footer">
           <div>
             <a href="https://www.linkedin.com/in/safwan-taleb/">MyBio</a>
             <span>&copy; 2021 creative_Labs.</span>
           </div>
-          <div class="ml-auto">
+          <div className="ml-auto">
             <span>
               Powered by
               <i>
